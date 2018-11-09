531652fe3eddf8714528e47754f82ec2148b087a
Mikhailo Bidyak <sgame666@gmail.com>
2018-11-09 15:07:38 +0200

    The menu can be created by using recursion fuction.

--- Lab2_JS_Bootstrap/js/Menu.js
+++ Lab2_JS_Bootstrap/js/Menu.js
@@ -13,7 +13,7 @@ var myModule = (function() {
         {
             title: "Products >",
             url: "#",
-            child: [{
+            dropMenu: [{
                     title: "Strategy",
                     url: "#"
                 },
@@ -60,70 +60,45 @@ var myModule = (function() {
 
     ];
 
-    MAP.forEach(renderProductList);
-
-    function renderProductList(element, index, arr) {
+    function makeMenu(arr, i, element) {
         var li = document.createElement('li');
         li.setAttribute('class', 'nav-item');
 
-        if (MAP[index].child != null) {
-            li.setAttribute('id', 'menu');
+        element.appendChild(li);
+        li.innerHTML = "<a class='" + "nav-link" + "' href='" + arr[i].url + "'>" + arr[i].title + "</a>";
+
+        if (arr[i].dropMenu != null) {
+            var ul = document.createElement('ul');
+            ul.setAttribute('id', 'DropMenu');
+            li.setAttribute('id', 'DropElement');
+            li.appendChild(ul);
+            makeMenu(arr[i].dropMenu, 0, ul);
         }
 
-        ul.appendChild(li);
-
-        li.innerHTML = "<a class='" + "nav-link" + "' href='" + MAP[index].url + "'>" + MAP[index].title + "</a>";
-
-        if (MAP[index].child != null) {
-            dropDownMenu(index);
+        if (arr[i].verticalMenu != null) {
+            var ul = document.createElement('ul');
+            ul.setAttribute('id', 'VerticalMenu');
+            li.setAttribute('id', 'VerticalElement');
+            li.appendChild(ul);
+            makeMenu(arr[i].verticalMenu, 0, ul);
         }
-    }
 
-    function dropDownMenu(index) {
-        var ul = document.createElement('ul');
-        ul.setAttribute('id', 'DropDown');
-        document.getElementById('menu').appendChild(ul);
-
-        for (var iter = 0; iter < MAP[index].child.length; iter++) {
-            var li = document.createElement('li');
-            li.setAttribute('id', 'DropItem');
-            li.setAttribute('class', 'nav-item');
-            ul.appendChild(li);
-            li.innerHTML = "<a class='" + "nav-link" + "' href='" + MAP[index].child[iter].url + "'>" + MAP[index].child[iter].title + "</a>";
-
-            if (MAP[index].child[iter].verticalMenu != null) {
-                li.setAttribute('id', 'DropItemF');
-                verticalMenu(li, index, iter);
-            }
-        }
-    }
-
-    function verticalMenu(HTMLLIElement, index, iter) {
-        var ul = document.createElement('ul');
-        ul.setAttribute('id', 'VerticalMenu');
-        HTMLLIElement.appendChild(ul);
-
-        for (let vIter = 0; vIter < MAP[index].child[iter].verticalMenu.length; vIter++) {
-            var li = document.createElement('li');
-            li.setAttribute('id', 'VerticalItem');
-            li.setAttribute('class', 'nav-item');
-            ul.appendChild(li);
-            li.innerHTML = "<a class='" + "nav-link" + "' href='" + MAP[index].child[iter].verticalMenu[vIter].url + "'>" + MAP[index].child[iter].verticalMenu[vIter].title + "</a>";
-        }
+        i++;
+        if (i < arr.length) makeMenu(arr, i, element);
     }
 
     function dropDownListener() {
-        var dropMenu = document.getElementById('menu');
-        if (dropMenu)
-            dropMenu.addEventListener("mouseenter", openDropDownMenu);
+        var dropElement = document.getElementById('DropElement');
+        if (dropElement)
+            dropElement.addEventListener("mouseenter", openDropDownMenu);
 
         function openDropDownMenu() {
-            document.getElementById("DropDown").classList.toggle("active");
+            document.getElementById("DropMenu").classList.toggle("active");
         }
     }
 
     function vertMenuListener() {
-        var VertMenu = document.getElementById('DropItemF');
+        var VertMenu = document.getElementById('VerticalElement');
         if (VertMenu)
             VertMenu.addEventListener("mouseenter", openVerticalMenu);
 
@@ -131,11 +106,16 @@ var myModule = (function() {
             document.getElementById("VerticalMenu").classList.toggle("active")
         }
     }
+
     return {
         DownMenu: dropDownListener,
-        VerticalMenu: vertMenuListener
+        VerticalMenu: vertMenuListener,
+        Menu: makeMenu,
+        Array: MAP,
+        GlobalElement: ul
     };
 }());
 
+myModule.Menu(myModule.Array, 0, myModule.GlobalElement);
 myModule.DownMenu();
 myModule.VerticalMenu();
