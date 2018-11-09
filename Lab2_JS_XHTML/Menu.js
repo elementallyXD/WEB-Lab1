097f4820028deb5c13a2375a54c373cc34bebefa
Mikhailo Bidyak <sgame666@gmail.com>
2018-11-09 16:03:30 +0200

    The menu can be created by using recursion fuction.

--- Lab2_JS_XHTML/Menu.js
+++ Lab2_JS_XHTML/Menu.js
@@ -23,7 +23,7 @@ var myModule = (function() {
         {
             title: "Products >",
             url: "#",
-            child: [{
+            dropMenu: [{
                     title: "RPG >",
                     url: "#",
                     verticalMenu: [{
@@ -58,67 +58,67 @@ var myModule = (function() {
 
     ];
 
-    MAP.forEach(renderProductList);
+    /* MAP.forEach(renderProductList);
 
-    function renderProductList(element, index, arr) {
-        var li = document.createElement('li');
+     function renderProductList(element, index, arr) {
+         var li = document.createElement('li');
 
-        if (MAP[index].child != null) {
-            li.setAttribute('id', 'menu');
-        }
+         if (MAP[index].child != null) {
+             li.setAttribute('id', 'menu');
+         }
 
-        ul.appendChild(li);
+         ul.appendChild(li);
 
-        li.innerHTML = "<a href='" + MAP[index].url + "'>" + MAP[index].title + "</a>";
+         li.innerHTML = "<a href='" + MAP[index].url + "'>" + MAP[index].title + "</a>";
 
-        if (MAP[index].child != null) {
-            dropDownMenu(index);
-        }
-    }
+         if (MAP[index].child != null) {
+             dropDownMenu(index);
+         }
+     }
 
-    function dropDownMenu(index) {
-        var ul = document.createElement('ul');
-        ul.setAttribute('id', 'DropDown');
-        document.getElementById('menu').appendChild(ul);
+     function dropDownMenu(index) {
+         var ul = document.createElement('ul');
+         ul.setAttribute('id', 'DropDown');
+         document.getElementById('menu').appendChild(ul);
 
-        for (var iter = 0; iter < MAP[index].child.length; iter++) {
-            var li = document.createElement('li');
-            li.setAttribute('id', 'DropItem');
-            ul.appendChild(li);
-            li.innerHTML = "<a href='" + MAP[index].child[iter].url + "'>" + MAP[index].child[iter].title + "</a>";
+         for (var iter = 0; iter < MAP[index].child.length; iter++) {
+             var li = document.createElement('li');
+             li.setAttribute('id', 'DropItem');
+             ul.appendChild(li);
+             li.innerHTML = "<a href='" + MAP[index].child[iter].url + "'>" + MAP[index].child[iter].title + "</a>";
 
-            if (MAP[index].child[iter].verticalMenu != null) {
-                li.setAttribute('id', 'DropItemF');
-                verticalMenu(li, index, iter);
-            }
-        }
-    }
+             if (MAP[index].child[iter].verticalMenu != null) {
+                 li.setAttribute('id', 'DropItemF');
+                 verticalMenu(li, index, iter);
+             }
+         }
+     }
 
-    function verticalMenu(HTMLLIElement, index, iter) {
-        var ul = document.createElement('ul');
-        ul.setAttribute('id', 'VerticalMenu');
-        HTMLLIElement.appendChild(ul);
+     function verticalMenu(HTMLLIElement, index, iter) {
+         var ul = document.createElement('ul');
+         ul.setAttribute('id', 'VerticalMenu');
+         HTMLLIElement.appendChild(ul);
 
-        for (let vIter = 0; vIter < MAP[index].child[iter].verticalMenu.length; vIter++) {
-            var li = document.createElement('li');
-            li.setAttribute('id', 'VerticalItem');
-            ul.appendChild(li);
-            li.innerHTML = "<a href='" + MAP[index].child[iter].verticalMenu[vIter].url + "'>" + MAP[index].child[iter].verticalMenu[vIter].title + "</a>";
-        }
-    }
+         for (let vIter = 0; vIter < MAP[index].child[iter].verticalMenu.length; vIter++) {
+             var li = document.createElement('li');
+             li.setAttribute('id', 'VerticalItem');
+             ul.appendChild(li);
+             li.innerHTML = "<a href='" + MAP[index].child[iter].verticalMenu[vIter].url + "'>" + MAP[index].child[iter].verticalMenu[vIter].title + "</a>";
+         }
+     }*/
 
     function dropDownListener() {
-        var dropMenu = document.getElementById('menu');
-        if (dropMenu)
-            dropMenu.addEventListener("mouseenter", openDropDownMenu);
+        var dropElement = document.getElementById('DropElement');
+        if (dropElement)
+            dropElement.addEventListener("mouseenter", openDropDownMenu);
 
         function openDropDownMenu() {
-            document.getElementById("DropDown").classList.toggle("active");
+            document.getElementById("DropMenu").classList.toggle("active");
         }
     }
 
     function vertMenuListener() {
-        var VertMenu = document.getElementById('DropItemF');
+        var VertMenu = document.getElementById('VerticalElement');
         if (VertMenu)
             VertMenu.addEventListener("mouseenter", openVerticalMenu);
 
@@ -126,11 +126,42 @@ var myModule = (function() {
             document.getElementById("VerticalMenu").classList.toggle("active")
         }
     }
+
+    function makeMenu(arr, i, element) {
+        var li = document.createElement('li');
+
+        element.appendChild(li);
+        li.innerHTML = "<a href='" + arr[i].url + "'>" + arr[i].title + "</a>";
+
+        if (arr[i].dropMenu != null) {
+            var ul = document.createElement('ul');
+            ul.setAttribute('id', 'DropMenu');
+            li.setAttribute('id', 'DropElement');
+            li.appendChild(ul);
+            makeMenu(arr[i].dropMenu, 0, ul);
+        }
+
+        if (arr[i].verticalMenu != null) {
+            var ul = document.createElement('ul');
+            ul.setAttribute('id', 'VerticalMenu');
+            li.setAttribute('id', 'VerticalElement');
+            li.appendChild(ul);
+            makeMenu(arr[i].verticalMenu, 0, ul);
+        }
+
+        i++;
+        if (i < arr.length) makeMenu(arr, i, element);
+    }
+
     return {
         DownMenu: dropDownListener,
-        VerticalMenu: vertMenuListener
+        VerticalMenu: vertMenuListener,
+        Menu: makeMenu,
+        Array: MAP,
+        GlobalElement: ul
     };
 }());
 
+myModule.Menu(myModule.Array, 0, myModule.GlobalElement);
 myModule.DownMenu();
 myModule.VerticalMenu();
